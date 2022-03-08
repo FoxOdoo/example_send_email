1144babf2e4cb22cda5c74dc0e6a8f7a9a4bb585
FoxOdoo <76999365+FoxOdoo@users.noreply.github.com>
2022-03-08 16:56:01 +0100

    Add files via upload

---
+++ send_email_wizard/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Send Email con Wizard",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Flash",
+    'website': "https://www.flashodoo.com",
+    'category': 'Custom',
+    'version': '1.0',
+    'depends': [
+        'base',
+        'mail',
+        'contacts',
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+}
